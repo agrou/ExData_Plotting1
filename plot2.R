2131d1c019428a0bece0e2cd0a85f3b57c37d782
agrou <ac.ilustat@gmail.com>
2016-06-05 15:02:07 +0100

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,32 @@
+# Household Power Consumption Data  
+
+# Code to produce plot 2
+
+# Load the data 
+file <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if(!file.exists("./Household_data")){dir.create("./Household_data")}
+download.file(file, "./Household_data", method = "curl")
+if(!file.exists("household_power_consumption.txt")){unzip("./Household_data", "household_power_consumption.txt")}
+
+#Read the data
+data <- read.table("household_power_consumption.txt", header = TRUE, 
+                   sep = ";", stringsAsFactors = FALSE, dec = ".")
+
+# Subset the data for the 2-day period in February, 2007
+dataSub <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+
+# Convert Date and Time variables into date and times classes
+dates <- strptime(paste(dataSub$Date, dataSub$Time), "%d/%m/%Y %H:%M:%S")
+
+# Clean up the data
+names(dataSub)
+GAP <- as.numeric(dataSub$Global_active_power)
+
+# Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels
+
+# Plot 2
+png("plot2.png", width = 480, height = 480)
+par(mar = c(5, 5, 4, 2))
+with(dataSub, plot(dates, GAP, type = "l", ylab = "Global Active Power (kilowatts)", 
+                   xlab = ""))
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,38 @@
+# Household Power Consumption Data  
+
+# Code to produce plot 3
+
+# Load the data 
+file <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if(!file.exists("./Household_data")){dir.create("./Household_data")}
+download.file(file, "./Household_data", method = "curl")
+if(!file.exists("household_power_consumption.txt")){unzip("./Household_data", "household_power_consumption.txt")}
+
+#Read the data
+data <- read.table("household_power_consumption.txt", header = TRUE, 
+                   sep = ";", stringsAsFactors = FALSE, dec = ".")
+
+# Subset the data for the 2-day period in February, 2007
+dataSub <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+
+# Convert Date and Time variables into date and times classes
+dates <- strptime(paste(dataSub$Date, dataSub$Time), "%d/%m/%Y %H:%M:%S")
+
+# Clean up the data
+names(dataSub)
+GAP <- as.numeric(dataSub$Global_active_power)
+SM_1 <- as.numeric(dataSub$Sub_metering_1)
+SM_2 <- as.numeric(dataSub$Sub_metering_2)
+SM_3 <- as.numeric(dataSub$Sub_metering_3)
+
+# Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels
+
+# Plot 3
+png("plot3.png", width = 480, height = 480)
+par(mar = c(5, 5, 2, 2))
+plot(dates, SM_1, type = "l", ylab = "Energy Submetering", xlab ="")
+lines(dates, SM_2, type = "l", col = "red")
+lines(dates, SM_3, type = "l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"), lty = 1, lwd = 2, box.lwd = 0.5)
+dev.off()
